8435b475937e9d7089e17737f26220a11a82679d
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-01-27 23:14:01 +0800

    Create UF.java

---
+++ courseraAlgorithm/src/UF.java
@@ -0,0 +1,21 @@
+public class UF {
+    public UF(int N) {
+
+    }
+
+    void union(int p, int q) {
+
+    }
+
+    boolean connect(int p, int q) {
+        return false;
+    }
+
+    int find(int p) {
+        return 0;
+    }
+
+    int count() {
+        return 0;
+    }
+}
