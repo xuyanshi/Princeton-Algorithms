8b439c73919128d5a5e2c74587d28faea374794d
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-01-29 12:04:37 +0800

    update

--- courseraAlgorithm/src/UF.java
+++ courseraAlgorithm/src/UF.java
@@ -1,3 +1,6 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class UF {
     public UF(int N) {
 
@@ -7,7 +10,7 @@ public class UF {
 
     }
 
-    boolean connect(int p, int q) {
+    boolean connected(int p, int q) {
         return false;
     }
 
@@ -17,5 +20,18 @@ public class UF {
 
     int count() {
         return 0;
+    }
+
+    public static void main(String[] args) {
+        int N = StdIn.readInt();
+        UF uf = new UF(N);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (!uf.connected(p, q)) {
+                uf.union(p, q);
+                StdOut.println(p + " " + q);
+            }
+        }
     }
 }
