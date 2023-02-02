57ce6ca8318b66897a7dbf84a4f14feb78c525b7
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-02-02 23:21:38 +0800

    queues init

---
+++ queues/src/Deque.java
@@ -0,0 +1,50 @@
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+
+    // construct an empty deque
+    public Deque() {
+
+    }
+
+    // is the deque empty?
+    public boolean isEmpty() {
+        return true;
+    }
+
+    // return the number of items on the deque
+    public int size() {
+        return 0;
+    }
+
+    // add the item to the front
+    public void addFirst(Item item) {
+
+    }
+
+    // add the item to the back
+    public void addLast(Item item) {
+
+    }
+
+    // remove and return the item from the front
+    public Item removeFirst() {
+        return null;
+    }
+
+    // remove and return the item from the back
+    public Item removeLast() {
+        return null;
+    }
+
+    // return an iterator over items in order from front to back
+    public Iterator<Item> iterator() {
+        return null;
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+
+    }
+
+}
