b318353b3f309500b53c72f3fc70feceb41b9bf3
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-02-02 23:54:28 +0800

    Naive Calculator

---
+++ courseraAlgorithm/src/Evaluate.java
@@ -0,0 +1,27 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+
+public class Evaluate {
+    public static void main(String[] args) {
+        Stack<String> ops = new Stack<>();
+        Stack<Double> vals = new Stack<>();
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+            if (s.equals("(")) {
+            } else if (s.equals("+") || s.equals("*")) {
+                ops.push(s);
+            } else if (s.equals(")")) {
+                String op = ops.pop();
+                if (op.equals("+")) {
+                    vals.push(vals.pop() + vals.pop());
+                } else if (op.equals("*")) {
+                    vals.push(vals.pop() * vals.pop());
+                }
+            } else {
+                vals.push(Double.parseDouble(s));
+            }
+        }
+        StdOut.println(vals.pop());
+    }
+}
