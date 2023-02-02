a9341dbf48c9574d5d928d9182af03c7955069a2
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-02-02 12:54:19 +0800

    Update Percolation.java

--- percolation/src/Percolation.java
+++ percolation/src/Percolation.java
@@ -27,57 +27,59 @@ public class Percolation {
         opened[bottom] = true;
     }
 
-//    private class UF {
-//        private int[] id; // component identifier
-//        private int[] sz; // size
-//        int cnt; // count
-//
-//        public UF(int N) {
-//            id = new int[N];
-//            sz = new int[N];
-//            for (int i = 0; i < N; i++) {
-//                id[i] = i;
-//                sz[i] = 1;
-//            }
-//            cnt = N;
-//        }
-//
-//        private int root(int i) {
-//            while (id[i] != i) {
-//                id[i] = id[id[i]]; // Path compression
-//                i = id[i];
-//            }
-//            return i;
-//        }
-//
-//        void union(int p, int q) {
-//            int rootP = root(p);
-//            int rootQ = root(q);
-//            if (rootP == rootQ) {
-//                return;
-//            }
-//            if (sz[rootP] < sz[rootQ]) {
-//                id[rootP] = rootQ;
-//                sz[rootQ] += sz[rootP];
-//            } else {
-//                id[rootQ] = rootP;
-//                sz[rootP] += sz[rootQ];
-//            }
-//            cnt--;
-//        }
-//
-//        boolean connected(int p, int q) {
-//            return root(p) == root(q);
-//        }
-//
-//        int find(int p) {
-//            return id[p];
-//        }
-//
-//        int count() {
-//            return cnt;
-//        }
-//    }
+/*
+    private class UF {
+        private int[] id; // component identifier
+        private int[] sz; // size
+        int cnt; // count
+
+        public UF(int N) {
+            id = new int[N];
+            sz = new int[N];
+            for (int i = 0; i < N; i++) {
+                id[i] = i;
+                sz[i] = 1;
+            }
+            cnt = N;
+        }
+
+        private int root(int i) {
+            while (id[i] != i) {
+                id[i] = id[id[i]]; // Path compression
+                i = id[i];
+            }
+            return i;
+        }
+
+        void union(int p, int q) {
+            int rootP = root(p);
+            int rootQ = root(q);
+            if (rootP == rootQ) {
+                return;
+            }
+            if (sz[rootP] < sz[rootQ]) {
+                id[rootP] = rootQ;
+                sz[rootQ] += sz[rootP];
+            } else {
+                id[rootQ] = rootP;
+                sz[rootP] += sz[rootQ];
+            }
+            cnt--;
+        }
+
+        boolean connected(int p, int q) {
+            return root(p) == root(q);
+        }
+
+        int find(int p) {
+            return id[p];
+        }
+
+        int count() {
+            return cnt;
+        }
+    }
+*/
 
     private boolean illegal(int coordinate) {
         return coordinate < 1 || coordinate > N;
