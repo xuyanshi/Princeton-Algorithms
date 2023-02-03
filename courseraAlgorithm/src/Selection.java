88fcb817f8241b33e39dffdb7fa5fb4d4678bd1d
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-02-03 19:21:23 +0800

    selection sort

---
+++ courseraAlgorithm/src/Selection.java
@@ -0,0 +1,47 @@
+public class Selection {
+    public static void sort(Comparable[] a) {
+        int n = a.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (less(a[j], a[min])) {
+                    min = j;
+                }
+            }
+            exchange(a, min, i);
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exchange(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    private static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void printArray(Comparable[] a) {
+        for (Comparable element : a) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Integer[] a = new Integer[]{2, 4, 3, 5, 1};
+        printArray(a);
+        Selection.sort(a);
+        printArray(a);
+    }
+}
