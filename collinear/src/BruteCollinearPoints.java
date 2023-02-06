53120d204257852055485e0570cbe89e8c7fd572
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-02-07 00:15:01 +0800

    Update BruteCollinearPoints.java

--- collinear/src/BruteCollinearPoints.java
+++ collinear/src/BruteCollinearPoints.java
@@ -32,17 +32,19 @@ public class BruteCollinearPoints {
             throw new IllegalArgumentException("should include each line segment containing 4 points exactly once.");
         }
 
-        Arrays.sort(points);
+        Point[] tmp = new Point[n];
+        System.arraycopy(points, 0, tmp, 0, n);
+        Arrays.sort(tmp);
         ls_lst = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 for (int k = j + 1; k < n; k++) {
                     for (int l = k + 1; l < n; l++) {
-                        double s1 = points[i].slopeTo(points[j]);
-                        double s2 = points[i].slopeTo(points[k]);
-                        double s3 = points[i].slopeTo(points[l]);
+                        double s1 = tmp[i].slopeTo(tmp[j]);
+                        double s2 = tmp[i].slopeTo(tmp[k]);
+                        double s3 = tmp[i].slopeTo(tmp[l]);
                         if (s1 == s2 && s2 == s3) {
-                            ls_lst.add(new LineSegment(points[i], points[l]));
+                            ls_lst.add(new LineSegment(tmp[i], tmp[l]));
                         }
                     }
                 }
