b7cb4c849f3fee8319dcd5b486959885b7fec826
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-02-14 16:36:12 +0800

    Create maxPQ.java

---
+++ courseraAlgorithm/src/maxPQ.java
@@ -0,0 +1,34 @@
+public class maxPQ<Key extends Comparable<Key>> {
+    public maxPQ() {
+
+
+    }
+
+    public maxPQ(Key[] a) {
+
+    }
+
+    void insert(Key v) {
+
+    }
+
+    Key delMax() {
+        return null;
+    }
+
+    boolean isEmpty() {
+        return size() > 0;
+
+    }
+
+    Key max() {
+        return null;
+    }
+
+
+    int size() {
+        return 0;
+    }
+
+
+}
