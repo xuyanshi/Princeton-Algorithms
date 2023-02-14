1a85a2e0f5671ccba38c86ee0547ef3cd840186f
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-02-14 17:51:22 +0800

    Update maxPQ.java

--- courseraAlgorithm/src/maxPQ.java
+++ courseraAlgorithm/src/maxPQ.java
@@ -1,6 +1,11 @@
 public class maxPQ<Key extends Comparable<Key>> {
-    public maxPQ() {
 
+    private Key[] pq;
+    private int N;
+
+    public maxPQ(int capacity) {
+        pq = (Key[]) new Object[capacity + 1];
+        N = 0;
 
     }
 
@@ -30,5 +35,7 @@ public class maxPQ<Key extends Comparable<Key>> {
         return 0;
     }
 
-
+    private boolean less(int i, int j) {
+        return pq[i].compareTo(pq[j]) < 0;
+    }
 }
