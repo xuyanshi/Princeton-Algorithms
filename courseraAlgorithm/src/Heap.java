eb3147624c49c88010ee2f05fe5989c91cc612e5
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-02-15 19:59:07 +0800

    Update Heap.java

--- courseraAlgorithm/src/Heap.java
+++ courseraAlgorithm/src/Heap.java
@@ -1,3 +1,45 @@
+import java.util.Arrays;
+
 public class Heap {
-    
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        for (int k = N / 2; k > 0; k--) {
+            sink(a, k, N);
+        }
+        while (N > 1) {
+            exch(a, 1, N--);
+            sink(a, 1, N);
+        }
+    }
+
+    private static void sink(Comparable[] a, int k, int N) {
+        while (2 * k <= N) {
+            int j = 2 * k;
+            if (j < N && less(a, j, j + 1)) {
+                j++;
+            }
+            if (!less(a, k, j)) {
+                break;
+            }
+            exch(a, k, j);
+            k = j;
+        }
+    }
+
+    private static boolean less(Comparable[] a, int i, int j) {
+        return a[i - 1].compareTo(a[j - 1]) < 0;
+    }
+
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable tmp = a[i - 1];
+        a[i - 1] = a[j - 1];
+        a[j - 1] = tmp;
+    }
+
+    public static void main(String[] args) {
+        Integer[] a = new Integer[]{3, 5, 1, 7, 6, 2, 4, 0};
+        Heap.sort(a);
+        System.out.println(Arrays.toString(a));
+    }
 }
