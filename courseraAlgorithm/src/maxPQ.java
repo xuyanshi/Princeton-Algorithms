bf9a27bdfaef14cd785625d7ca4517aadbcef28d
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-02-15 19:25:13 +0800

    update

--- courseraAlgorithm/src/maxPQ.java
+++ courseraAlgorithm/src/maxPQ.java
@@ -4,21 +4,29 @@ public class maxPQ<Key extends Comparable<Key>> {
     private int N;
 
     public maxPQ(int capacity) {
-        pq = (Key[]) new Object[capacity + 1];
+        pq = (Key[]) new Comparable[capacity + 1];
         N = 0;
-
     }
 
     public maxPQ(Key[] a) {
-
+        pq = (Key[]) new Comparable[2 * a.length + 1];
+        for (Key v : a) {
+            insert(v);
+        }
+        N = a.length;
     }
 
     void insert(Key v) {
-
+        pq[++N] = v;
+        swim(N);
     }
 
     Key delMax() {
-        return null;
+        Key maxItem = pq[1];
+        exch(1, N--);
+        sink(1);
+        pq[N + 1] = null;
+        return maxItem;
     }
 
     boolean isEmpty() {
@@ -27,6 +35,9 @@ public class maxPQ<Key extends Comparable<Key>> {
     }
 
     Key max() {
+        if (!isEmpty()) {
+            return pq[1];
+        }
         return null;
     }
 
@@ -36,11 +47,24 @@ public class maxPQ<Key extends Comparable<Key>> {
     }
 
     private void swim(int k) {
-
+        while (k > 1 && less(k / 2, k)) {
+            exch(k, k / 2);
+            k /= 2;
+        }
     }
 
     private void sink(int k) {
-
+        while (2 * k < N) {
+            int j = 2 * k;
+            if (less(2 * j, 2 * j + 1)) {
+                j++;
+            }
+            if (!less(k, j)) {
+                break;
+            }
+            exch(k, j);
+            k = j;
+        }
     }
 
     private boolean less(int i, int j) {
