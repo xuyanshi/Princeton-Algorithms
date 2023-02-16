90dc6fe18f2eb516722c627c3879cff08c348af4
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-02-16 08:44:38 +0800

    test cases are not completed

--- puzzle8/src/Solver.java
+++ puzzle8/src/Solver.java
@@ -1,4 +1,6 @@
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.StdOut;
 
 public class Solver {
 
@@ -28,27 +30,27 @@ public class Solver {
 
     // test client (see below)
     public static void main(String[] args) {
-//
-//        // create initial board from file
-//        In in = new In(args[0]);
-//        int n = in.readInt();
-//        int[][] tiles = new int[n][n];
-//        for (int i = 0; i < n; i++)
-//            for (int j = 0; j < n; j++)
-//                tiles[i][j] = in.readInt();
-//        Board initial = new Board(tiles);
-//
-//        // solve the puzzle
-//        Solver solver = new Solver(initial);
-//
-//        // print solution to standard output
-//        if (!solver.isSolvable())
-//            StdOut.println("No solution possible");
-//        else {
-//            StdOut.println("Minimum number of moves = " + solver.moves());
-//            for (Board board : solver.solution())
-//                StdOut.println(board);
-//        }
+
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] tiles = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                tiles[i][j] = in.readInt();
+        Board initial = new Board(tiles);
+
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+
+        // print solution to standard output
+        if (!solver.isSolvable())
+            StdOut.println("No solution possible");
+        else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution())
+                StdOut.println(board);
+        }
     }
 
 }
