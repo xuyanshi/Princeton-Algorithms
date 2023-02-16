abb696f55a93643f3f2327b091c078716e929c02
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-02-16 23:07:38 +0800

    Update BST

--- courseraAlgorithm/src/BST.java
+++ courseraAlgorithm/src/BST.java
@@ -16,10 +16,28 @@ public class BST<Key extends Comparable<Key>, Value> {
     }
 
     public void put(Key key, Value val) {
+        root = put(root, key, val);
+    }
 
+    private Node put(Node x, Key key, Value val) {
+        if (x == null) return new Node(key, val);
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0)
+            x.left = put(x.left, key, val);
+        else if (cmp > 0)
+            x.right = put(x.right, key, val);
+        else x.val = val;
+        return x;
     }
 
     public Value get(Key key) {
+        Node x = root;
+        while (x != null) {
+            int cmp = key.compareTo(x.key);
+            if (cmp < 0) x = x.left;
+            else if (cmp > 0) x = x.right;
+            else return x.val;
+        }
         return null;
     }
 
