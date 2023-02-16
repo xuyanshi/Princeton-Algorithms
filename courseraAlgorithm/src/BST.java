0877dd7b56108414db8a8808ceb95c2ba63004b6
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-02-16 22:58:06 +0800

    Binary Search Tree

---
+++ courseraAlgorithm/src/BST.java
@@ -0,0 +1,33 @@
+/* Binary Search Tree */
+
+
+public class BST<Key extends Comparable<Key>, Value> {
+    private Node root;
+
+    private class Node {
+        private Key key;
+        private Value val;
+        private Node left, right;
+
+        public Node(Key key, Value val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+
+    public void put(Key key, Value val) {
+
+    }
+
+    public Value get(Key key) {
+        return null;
+    }
+
+    public void delete(Key key) {
+
+    }
+
+    public Iterable<Key> iterator() {
+        return null;
+    }
+}
