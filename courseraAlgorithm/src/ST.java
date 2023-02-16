c15f20dfc8eab5316b88c514d9c23e742a6bd1a9
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-02-16 22:53:42 +0800

    Update ST

--- courseraAlgorithm/src/ST.java
+++ courseraAlgorithm/src/ST.java
@@ -1,13 +1,34 @@
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
-public class ST<Key, Value> {
+public class ST<Key extends Comparable<Key>, Value> {
+    private int N;
+    private Key[] keys;
+    private Value[] vals;
+
     void put(Key key, Value val) {
 
     }
 
     Value get(Key key) {
-        return null;
+        if (isEmpty()) {
+            return null;
+        }
+        int i = rank(key);
+        if (i < N && keys[i].compareTo(key) == 0) return vals[i];
+        else return null;
+    }
+
+    private int rank(Key key) {
+        int lo = 0, hi = N - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            int cmp = key.compareTo(keys[mid]);
+            if (cmp < 0) hi = mid - 1;
+            else if (cmp > 0) lo = mid + 1;
+            else if (cmp == 0) return mid;
+        }
+        return lo;
     }
 
     void delete(Key key) {
