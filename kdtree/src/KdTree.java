b4c02995559ceaae82ad379406eadaff488a4a85
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-02-18 20:44:58 +0800

    Update KdTree.java

--- kdtree/src/KdTree.java
+++ kdtree/src/KdTree.java
@@ -2,10 +2,10 @@ import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.StdDraw;
 
+import java.util.ArrayList;
 import java.util.TreeSet;
 
 public class KdTree {
-
     TreeSet<Point2D> treeset;
 
     // construct an empty set of points
@@ -56,7 +56,13 @@ public class KdTree {
         if (rect == null) {
             throw new IllegalArgumentException("argument is null.");
         }
-        return null;
+        ArrayList<Point2D> pts = new ArrayList<>();
+        for (Point2D p : treeset) {
+            if (p.x() <= rect.xmax() && p.x() >= rect.xmin() && p.y() <= rect.ymax() && p.y() >= rect.ymin()) {
+                pts.add(p);
+            }
+        }
+        return pts;
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty
@@ -64,7 +70,21 @@ public class KdTree {
         if (p == null) {
             throw new IllegalArgumentException("argument is null.");
         }
-        return null;
+        if (isEmpty()) {
+            return null;
+        }
+        double minDistanceSquare = Double.MAX_VALUE;
+        Point2D minDistancePoint = null;
+        for (Point2D point : treeset) {
+            if (p == point) {
+                continue;
+            }
+            if (p.distanceSquaredTo(point) < minDistanceSquare) {
+                minDistanceSquare = p.distanceSquaredTo(point);
+                minDistancePoint = point;
+            }
+        }
+        return minDistancePoint;
     }
 
     // unit testing of the methods (optional)
