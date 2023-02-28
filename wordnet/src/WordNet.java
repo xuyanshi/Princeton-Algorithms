3df1685ac5d33fd31f3f5132980b934e95068687
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-02-28 08:30:01 +0800

    WordNet init

---
+++ wordnet/src/WordNet.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class WordNet {
+
+    // constructor takes the name of the two input files
+    public WordNet(String synsets, String hypernyms) {
+
+    }
+
+    // returns all WordNet nouns
+    public Iterable<String> nouns() {
+        ArrayList<String> wordNetNouns = new ArrayList<>();
+        return wordNetNouns;
+    }
+
+    // is the word a WordNet noun?
+    public boolean isNoun(String word) {
+        return word.isEmpty();
+    }
+
+    // distance between nounA and nounB (defined below)
+    public int distance(String nounA, String nounB) {
+        return 0;
+    }
+
+    // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
+    // in a shortest ancestral path (defined below)
+    public String sap(String nounA, String nounB) {
+        return null;
+    }
+
+    // do unit testing of this class
+    public static void main(String[] args) {
+
+    }
+}
