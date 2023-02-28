8873d9cc951dd35de6274216a03370434afd4df5
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-02-28 08:31:56 +0800

    update SAP

--- wordnet/src/SAP.java
+++ wordnet/src/SAP.java
@@ -1,4 +1,7 @@
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 
 public class SAP {
 
@@ -29,7 +32,16 @@ public class SAP {
 
     // do unit testing of this class
     public static void main(String[] args) {
-       
+        In in = new In(args[0]);
+        Digraph G = new Digraph(in);
+        SAP sap = new SAP(G);
+        while (!StdIn.isEmpty()) {
+            int v = StdIn.readInt();
+            int w = StdIn.readInt();
+            int length = sap.length(v, w);
+            int ancestor = sap.ancestor(v, w);
+            StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+        }
     }
 }
 
